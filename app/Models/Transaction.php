16e0be47e8fc52f5e5d06c3e9896ebca89e99df5
eldzn <daniil_sokolov.26@mail.ru>
2025-09-07 19:08:06 +0300

    Models: create Category and Transaction models with Eloquent relationships

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+      'amount',
+      'type',
+      'date',
+      'description',
+      'user_id',   
+      'category_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
